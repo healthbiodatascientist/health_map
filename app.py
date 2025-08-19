6ab9e101d27ffff9fa81c6b779d2540a9f86c855
healthbiodatascientist <susan.rarity-middleton@outlook.com>
2025-08-19 17:12:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -20,20 +20,90 @@ import folium
 # In[2]:
 
 
-df_hb_beds_filter = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/health_map/refs/heads/main/HealthBoardDataFinal.csv')
+def table_clean():
+    df_hosp_beds = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/Health-Dash/refs/heads/main/beds_by_nhs_board-of-treatment_specialty.csv') # Open Public Health Scotland Hospital Beds Data
+    df_map = read_dataframe('https://github.com/healthbiodatascientist/Health-Dash/raw/refs/heads/main/SG_NHS_HealthBoards_2019.shp') # Scottish Health Board Region Names and Geometries
+    df_ae = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/Health-Dash/refs/heads/main/weekly_ae_activity_20250615.csv') # Public Health Scotland's A&E data 2015-2025
+    df_emerge = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/Health-Dash/refs/heads/main/table-3-multiple-emergency-admissions-2023-24.csv', dtype = 'str') # Public Health Scotland Emergency Admissions data 2023-24
+    df_amb_turnaround = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/Health-Dash/refs/heads/main/median_ambulance_turnaround_times_2023_24.csv') # Scottish Ambulance Service data on turnaround times
+    df_amb_demand = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/Health-Dash/refs/heads/main/percentage_unscheduled_emergencies_attended_conveyed.csv')
+    df_ae['WeekEndingDate'] = df_ae['WeekEndingDate'].astype(str) # change WeekEndingDate to string for filtering later
+    df_hb_beds = df_hosp_beds.filter(items=['HB', 'FinancialYear', 'SpecialtyName', 'PercentageOccupancy', 'AverageAvailableStaffedBeds', 'AllStaffedBeds']) # filter
+    df_map = df_map.filter(items=['HBCode', 'HBName', 'geometry']) # filter for the columns we need
+    df_map = df_map.set_index('HBCode') # set the index to the Health Board Code
+    df_hb_beds_map = df_map.join(df_hb_beds.set_index('HB'), on='HBCode') # Join the tables
+    df_hb_beds_map = df_hb_beds_map.filter(items=['FinancialYear', 'SpecialtyName', 'HBName', 'PercentageOccupancy', 'AverageAvailableStaffedBeds', 'AllStaffedBeds', 'geometry']) 
+    df_hb_beds_filter = df_hb_beds_map.loc[df_hb_beds_map['FinancialYear'].str.startswith('2023', na=False)] # filter for year
+    df_hb_beds_filter = df_hb_beds_filter.loc[df_hb_beds_filter['SpecialtyName'] == 'All Acute Specialties']
+    df_ae = df_ae.loc[df_ae['AttendanceCategory'] == 'All'] # filter for 'All' attendances only
+    mask = (df_ae['WeekEndingDate'] > '20230402') & (df_ae['WeekEndingDate'] <= '20240331')
+    df_filter_year = df_ae.loc[mask] # filter for year
+    df_filternumber = df_filter_year.filter(items=['HBT', 'NumberOfAttendancesEpisode', 'NumberWithin4HoursEpisode', 'NumberOver4HoursEpisode', 'NumberOver8HoursEpisode', 'NumberOver12HoursEpisode']) # filter for numbers which can be summed only
+    df_hb = df_filternumber.groupby('HBT').sum() # group by health board
+    df_hb['PercentageWithin4HoursEpisode'] = df_hb['NumberWithin4HoursEpisode']/df_hb['NumberOfAttendancesEpisode']*100 # add in columns for percentages
+    df_hb['PercentageOver4HoursA&E'] = df_hb['NumberOver4HoursEpisode']/df_hb['NumberOfAttendancesEpisode']*100
+    df_hb['PercentageOver8HoursA&E'] = df_hb['NumberOver8HoursEpisode']/df_hb['NumberOfAttendancesEpisode']*100
+    df_hb['PercentageOver12HoursA&E'] = df_hb['NumberOver12HoursEpisode']/df_hb['NumberOfAttendancesEpisode']*100
+    df_hb_beds_filter = df_hb_beds_filter.join(df_hb, on=df_hb.index) # Join the tables
+    df_hb_beds_filter = df_hb_beds_filter.round(2)
+    df_hb_beds_filter = df_hb_beds_filter.filter(items=['HBName','PercentageOccupancy', 'PercentageWithin4HoursEpisode', 'PercentageOver4HoursA&E', 'PercentageOver8HoursA&E', 'PercentageOver12HoursA&E', 'geometry'])
+    df_hb_beds_filter.columns = ['HB Name','Percentage Occupancy Acute Beds', 'Percentage Within 4 Hours A&E', 'Percentage Over 4 Hours A&E', 'Percentage Over 8 Hours A&E', 'Percentage Over 12 Hours A&E', 'geometry']
+    df_emerge_NHS = df_emerge.loc[df_emerge['lookup'].str.contains('NHS')] # filter for NHS Boards
+    df_emerge_NHS = df_emerge_NHS.loc[df_emerge_NHS['lookup'].str.contains('All Sexes - All AgesTotal')] # filter for all sexes and ages totals
+    df_emerge_NHS = df_emerge_NHS.loc[df_emerge_NHS['lookup'].str.startswith('2023')] # filter for 2023/24
+    df_emerge_NHS = df_emerge_NHS.reset_index(drop=True)
+    df_emerge_NHS_region = df_emerge_NHS['lookup'].str.split('NHS', expand=True)
+    df_emerge_NHS_region_final = df_emerge_NHS_region[2].str.split('All Sexes', expand=True)
+    df_emerge_NHS_region_final = df_emerge_NHS_region_final[0]
+    df_emerge_NHS_region_final = pd.DataFrame(df_emerge_NHS_region_final)
+    df_emerge_NHS['lookup'] = df_emerge_NHS_region_final
+    df_emerge_NHS = df_emerge_NHS.dropna()
+    df_emerge_NHS = df_emerge_NHS.filter(items = ['lookup', 'patients_Rate', 'stays_Rate', 'los_stays_Rate'])
+    df_emerge_NHS.columns = ['region', 'Emergency Patients Rate', 'Emergency Stays Rate', 'Length Emergency Stays Rate']
+    df_emerge_NHS['region'] = df_emerge_NHS['region'].str.replace('&', 'and')
+    df_hb_beds_filter = df_hb_beds_filter.sort_values(by='HB Name')
+    df_emerge_NHS = df_emerge_NHS.set_index(df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.join(df_emerge_NHS, on=df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.filter(items=['HB Name','Percentage Occupancy Acute Beds', 'Percentage Within 4 Hours A&E', 'Percentage Over 4 Hours A&E', 'Percentage Over 8 Hours A&E', 'Percentage Over 12 Hours A&E', 'Emergency Patients Rate', 'Emergency Stays Rate', 'Length Emergency Stays Rate', 'geometry'])
+    df_amb_turnaround['2023_24_median_turnaround_time_(hh:mm:ss)'] = df_amb_turnaround['2023_24_median_turnaround_time_(hh:mm:ss)'].astype(str)
+    df_amb_turnaround['2023_24_median_turnaround_time_(hh:mm:ss)'] = df_amb_turnaround['2023_24_median_turnaround_time_(hh:mm:ss)'].str.split(':', expand=True)[1]
+    df_amb_turnaround.columns = ['HealthBoard', 'Median Ambulance Turnaround Time (min)']
+    df_amb_turnaround = df_amb_turnaround.set_index(df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.join(df_amb_turnaround, on=df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.filter(items=['HB Name','Percentage Occupancy Acute Beds', 'Percentage Within 4 Hours A&E', 'Percentage Over 4 Hours A&E', 'Percentage Over 8 Hours A&E', 'Percentage Over 12 Hours A&E', 'Emergency Patients Rate', 'Emergency Stays Rate', 'Length Emergency Stays Rate', 'Median Ambulance Turnaround Time (min)', 'geometry'])
+    df_amb_demand.columns = ['HealthBoard', 'NumEmergencies', 'EmergeciesAttended', 'EmergenciesConveyed', 'Percentage Emergencies Attended', 'Percentage Emergencies Conveyed']
+    df_amb_demand = df_amb_demand.set_index(df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.join(df_amb_demand, on=df_hb_beds_filter.index)
+    df_hb_beds_filter = df_hb_beds_filter.filter(items=['HB Name', 'Percentage Emergencies Conveyed', 'Median Ambulance Turnaround Time (min)', 'Emergency Patients Rate', 'Percentage Within 4 Hours A&E', 'Percentage Over 4 Hours A&E', 'Percentage Over 8 Hours A&E', 'Percentage Over 12 Hours A&E', 'Percentage Occupancy Acute Beds', 'Length Emergency Stays Rate', 'geometry'])
+    df_hb_beds_filter.to_csv('HealthBoardDataFinal.csv')
+    return df_hb_beds_filter
+df_hb_beds_filter = table_clean()
+
+
+# In[6]:
+
+
 def no_geometry():
-    df_hb_beds_filter = pd.read_csv('https://raw.githubusercontent.com/healthbiodatascientist/health_map/refs/heads/main/HealthBoardDataFinal.csv')
+    df_hb_beds_filter = table_clean()
     df_hb_beds_table = df_hb_beds_filter.drop('geometry', axis=1)
     df_hb_beds_table['Emergency Patients Rate'] = df_hb_beds_table['Emergency Patients Rate'].astype(float)
     df_hb_beds_table['Median Ambulance Turnaround Time (min)'] = df_hb_beds_table['Median Ambulance Turnaround Time (min)'].astype(int)
     df_hb_beds_table['Length Emergency Stays Rate'] = df_hb_beds_table['Length Emergency Stays Rate'].astype(float)
-    df_hb_beds_table = df_hb_beds_table.set_index('HBCode')
     return df_hb_beds_table
 df_hb_beds_table = no_geometry()
 df_numeric_columns = df_hb_beds_table.select_dtypes('number')
+def create_map():
+    # imports, filters and joins the data and produces the map
+    df_hb_beds_filter = table_clean()
+    foliummap = folium.Map(location=[55.941457, -3.205744], zoom_start=6)
+    mapped = df_hb_beds_filter.explore(m=foliummap, column='HB Name')
+    mapped.save('foliummap.html')
+
+    return mapped
+create_map()
 
 
-# In[3]:
+# In[7]:
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -90,18 +160,9 @@ app.layout = dbc.Container([
     html.Link(href="https://www.scottishambulance.com/publications/previous-unscheduled-care-operational-statistics/", rel="stylesheet"),
     html.Link(href="https://www.scottishambulance.com/media/teapv5hp/foi-24-296-service-target-times.pdf", rel="stylesheet"),
     ])
-    
-def create_map():
-    # imports, filters and joins the data and produces the map
-    df_hb_beds_filter = table_clean()
-    foliummap = folium.Map(location=[55.941457, -3.205744], zoom_start=6)
-    mapped = df_hb_beds_filter.explore(m=foliummap, column='HB Name')
-    mapped.save('foliummap.html')
-
-    return mapped
 
 
-# In[4]:
+# In[8]:
 
 
 if __name__ == "__main__":
